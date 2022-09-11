599bf2df81bf0fc2eac63d5097e08d9b4b23f1b9
Erwin <57144439+n0rbb@users.noreply.github.com>
2022-09-11 10:56:40 +0200

    Counter fix

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-
-
 function daysDifference($startDate, $endDate) {
     oneDay = 24*60*60*1000;
     return Math.ceil(($endDate.getTime() - $startDate.getTime()) / oneDay - 1);  
 }
-      
-// month goes from 0-11
-$startDate = new Date(2022, 7, 29);
-$today = new Date();
-document.getElementById("counter").innerHTML = daysDifference($startDate, $today);
+
+function count() {
+    $startDate = new Date(2022, 7, 29);
+    $today = new Date();
+    document.getElementById("counter").innerHTML = daysDifference($startDate, $today);
+}
