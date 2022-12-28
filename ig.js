e7c23e38ecf5aee136f2a08cb0be1dea29ee2331
Erwin <57144439+n0rbb@users.noreply.github.com>
2022-12-28 14:22:27 +0100

    igcm

---
+++ ig.js
@@ -0,0 +1,10 @@
+function daysDifference($startDate, $endDate) {
+    oneDay = 24*60*60*1000;
+    return Math.ceil(($endDate.getTime() - $startDate.getTime()) / oneDay - 1);
+}
+
+function countig(){
+    $startDateIg = new Date(2022, 11, 28);
+    $today = new Date();
+    document.getElementById("counterig").innerHTML = daysDifference($startDateIg, $today);
+}
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function daysDifference($startDate, $endDate) {
     oneDay = 24*60*60*1000;
-    return Math.ceil(($endDate.getTime() - $startDate.getTime()) / oneDay - 1);  
+    return Math.ceil(($endDate.getTime() - $startDate.getTime()) / oneDay - 1);
 }
 
 function count() {
-    $startDate = new Date(2022, 8, 5);
+    $startDateTw = new Date(2022, 8, 5);
     $today = new Date();
-    document.getElementById("counter").innerHTML = daysDifference($startDate, $today);
+    document.getElementById("counter").innerHTML = daysDifference($startDateTw, $today);
 }
 
 function loss() {
